40ef0023ee53a89fb87da7d5fd7bc606c4787d3b
Austin Longo <alongo117@gmail.com>
2012-10-21 19:08:00 -0600

    Created the LegalMoves class w/o implementation.

---
+++ player/LegalMoves.java
@@ -0,0 +1,24 @@
+/* LegalMoves.java */
+
+package player; 
+
+/**
+ *	A public class with its own internal copy of the board.  
+ *	It contains static methods that can return all possible moves for a certain
+ *	player, and also whether a move is valid or not. 
+ *
+ *
+ */
+
+class LegalMoves{
+
+	static int[][] possibleMoves(int[][] board){
+		int[][] valid = new int[8][8];
+		
+		return valid;
+	}
+
+	static boolean isLegal(int x, int y){
+		return false;
+	}
+}
