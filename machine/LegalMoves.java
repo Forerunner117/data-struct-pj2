c786fa4146b402282bcb004c19f311af5b79c952
Austin Longo <alongo117@gmail.com>
2012-10-25 21:31:55 -0600

    Partially implemented the isLegal method from the LegalMoves class

--- machine/LegalMoves.java
+++ machine/LegalMoves.java
@@ -15,9 +15,47 @@ class LegalMoves{
 	// parameters and returns true for a legal move and false for an illegal
 	// move.
 	static boolean isLegal(Move m, int col){
+		if(m.moveKind == m.QUIT)
+			return true;
+
+		//checks if the move is on the board
+		if(!isOnBoard(m.x1, m.y1))
+			return false;
+
+		//checks for the corners
+		if((m.x1 == 0 && m.y1 == 0) || (m.x1 == 0 && m.y1 == 7) || (m.x1 == 7 && 
+		   m.y1 == 0) || (m.x1 == 7 && m.y1 == 7)){
+			return false;
+		}
+
+		//checks if a color is in the opponent's goal
+		if((m.x1 == 0 || m.x1 == 7) && col == Chip.BLACK)
+			return false;
+		if((m.y1 == 0 || m.y1 == 7) && col == Chip.WHITE)
+			return false;
+
+		//checks if the slot is filled
+		if(Board.board[m.x1][m.y1] != Board.EMPTY)
+			return false;
+
+		//checks if there are 10 BLACK or WHITE pieces and determines if the
+		//moveKind is accurate
+		if(Board.numPieces[col] > 9 && m.moveKind == m.ADD)
+			return false;
+		if(Board.numPieces[col] < 10 && m.moveKind == m.STEP)
+			return false;
+		
+		if(m.moveKind == m.STEP)
+					
+
 		return false;
 	}
 
+	// A static boolean class that returns true if the move is on the board and
+	// false if not.
+	static boolean isOnBoard(int x, int y){
+		return(x >= 0 && x <= 7 && y >= 0 && y <= 7);
+	}
 	
  	// A package protected static method that takes the color of the current player
  	// and returns a Board object consisting of null pointers (invalid moves) and
