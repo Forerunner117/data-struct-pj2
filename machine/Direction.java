ece5b31099f492198730d7a2da2e86c7dc211f01
Chris Costello <christopher.costello6@gmail.com>
2012-11-06 22:42:38 -0700

    Changed direction from an int to an enum

--- machine/Direction.java
+++ machine/Direction.java
@@ -1,4 +1,4 @@
-/*Direction.java*/
+/*java*/
 package machine; 
 /*
 	This is a container for direction values used in the explore method. 
@@ -7,41 +7,42 @@ package machine;
 */
 import player.*;
 
-public class Direction{
-	public final static int N = 1;
+public enum Direction{
+        N, NE, E, SE, S ,SW, W, NW;
+	/*public final static int N = 1;
 	public final static int NE = 2;
 	public final static int E = 3;
 	public final static int SE = 4;
 	public final static int S = 5;
 	public final static int SW = 6;
 	public final static int W = 7;
-	public final static int NW = 8;
-
-public static int getCurrDir(int i, int j){
+	public final static int NW = 8;*/
+// public Direction()
+public static Direction getCurrDir(int i, int j){
 	if( i == -1 && j == -1){
-                        return Direction.SW;
+                        return SW;
                 }	
                 if( i == -1 && j == 0){
-                        return Direction.W;
+                        return W;
                 }
                 if( i == -1 && j == 1){
-                        return Direction.NW;
+                        return NW;
                 }
                 if ( i == 0 && j == 1){
-                        return Direction.N;
+                        return N;
                 }
                 if (i == 1 && j == 1){
-                        return Direction.NE;
+                        return NE;
                 }
                 if( i == 1 && j == 0){
-                        return Direction.E;
+                        return E;
                 }
                 if( i == 1 && j == -1){
-                        return Direction.SE;
+                        return SE;
                 }
                 if ( i == 0 && j == -1){
-                        return Direction.S;
+                        return S;
                 }
-	return -1;//some number to satisfy the compiler
+	return N;//some number to satisfy the compiler
 	}
 }
